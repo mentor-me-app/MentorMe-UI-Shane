4ae862952855081a84f4af881297b95e3ada7778
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-05-22 15:20:12 -0700

    add js to index

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-const ctaButton = document.querySelector('.sign-up')
-console.log(ctaButton)
+//NavBar
+
+window.onscroll = function() {fixed()}
+const header = document.querySelector('header');
+const headerStick = header.offsetTop;
+function fixed(){
+    if(window.pageYOffset >= headerStick) {
+        header.classList.add('sticky')
+    } else {
+        header.classList.remove('sticky')
+    }
+}
+
+
+
+//Features Section 
+
+
+window.onscroll = function() {fadeIn()}
+const features = document.querySelector('.features-wrapper');
+const featureScroll = features.offsetTop;
+function fadeIn() {
+    if((window.pageYOffset + 300) >= featureScroll) {
+        features.classList.add('fadeIn');
+        features.classList.remove('hide');
+    } else {
+        features.classList.remove('fadeIn');
+        features.classList.add('hide')
+    }
+} 
+
+// $(features).scroll(() => {
+//     $('.features-content').fadeIn('slow');
+// })
+
