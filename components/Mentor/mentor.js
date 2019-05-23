92a660cb2e7e0594c4bae8c3bda5735c886728a7
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-05-22 22:41:30 -0700

    added scroll to index.html

---
+++ components/Mentor/mentor.js
@@ -0,0 +1,16 @@
+//Mentor Component
+// document.onscroll = function() {fadeIn()}
+// const mentors = document.querySelector('.top-mentors-wrapper');
+// const mentorScroll = mentors.offsetTop;
+// console.log(mentorScroll)
+// function fadeIn() {
+//     if(((window.pageYOffset + 100)) >= mentorScroll) {
+//         mentors.classList.add('fadeIn');
+//         mentors.classList.remove('hide');
+//     } else {
+//         mentors.classList.remove('fadeIn');
+//         mentors.classList.add('hide');
+//     }
+// } 
+
+//  mentors.addEventListener('scroll', fadeInn())
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function fixed(){
 //Features Section 
 
 
-window.onscroll = function() {fadeIn()}
+document.onscroll = function() {fadeIn()}
 const features = document.querySelector('.features-wrapper');
 const featureScroll = features.offsetTop;
 function fadeIn() {
@@ -25,11 +25,24 @@ function fadeIn() {
         features.classList.remove('hide');
     } else {
         features.classList.remove('fadeIn');
-        features.classList.add('hide')
+        features.classList.add('hide');
     }
 } 
 
-// $(features).scroll(() => {
-//     $('.features-content').fadeIn('slow');
-// })
+//How it Works
+
+const jump = document.querySelector('.color').onclick = function(){jumpTo()}
+
+function jumpTo() {
+    const howItWorks = document.querySelector('.slider-container');
+    howItWorks.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+}
+
+// const jumpTo = document.querySelector('.picSlider-wrapper').pageYOffset;
+// console.log(jumpTo)
+
+
+
+
+
 
